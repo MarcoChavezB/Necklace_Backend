88658901360a9cf27254276cbfe28a46bf954f11
Alonso <abrahamreynoso64@gmail.com>
2023-11-18 18:00:29 -0600

    Ruta de prueba

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,10 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::any('/ServerOn', function (){
+    return response()->json([
+        'message' => 'Conexion Establecida'
+    ]);
+});
