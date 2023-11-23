d95f9be50a8ec0520e4a3d99931f5c8afdac7fc3
Alonso <abrahamreynoso64@gmail.com>
2023-11-22 18:27:53 -0600

    Conflictos

--- database/migrations/2023_11_22_232740_create_devices_table.php
+++ database/migrations/2023_11_22_232740_create_devices_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::create('devices', function (Blueprint $table) {
-            $table->bigInteger('id')->autoIncrement(false)->primary();
+            $table->bigInteger('id')->primary();
             $table->string("dispositivo", 100);
             $table->timestamps();
         });
