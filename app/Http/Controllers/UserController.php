d947cbdac2a864e228368bde199742fe9d35583e
Alonso <abrahamreynoso64@gmail.com>
2023-11-29 21:55:54 -0600

    Como chingas marco

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -12,7 +12,7 @@ class UserController extends Controller
 {
 
     public function __construct(){
-        $this->middleware('auth:api', ['except' => ['register', 'login']]);
+        $this->middleware('auth:api', ['except' => ['register', 'login', 'getUserDevices']]);
     }
 
     public function login(){
