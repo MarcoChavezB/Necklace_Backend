c5eb8c37929474155a43c2bec2477bffe39b0555
Alonso <abrahamreynoso64@gmail.com>
2023-12-14 17:56:29 -0600

    Codigo para el actuador

---
+++ app/Http/Controllers/LedController.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Http\Controllers;
+
+
+use GuzzleHttp\Client;
+use Illuminate\Http\Request;
+
+class LedController extends Controller
+{
+    public function TurnOnLed($value){
+        $client = new Client();
+
+        try{
+
+            $response = $client->request('POST','https://io.adafruit.com/api/v2/MarcoChavez/feeds/led/data',[
+                'headers' => [
+                    'X-AIO-Key' => env('ADAFRUIT_IO_KEY')
+                ],
+                'json' => [
+                    'value' => $value
+                ]
+            ]);
+            return response()->json([
+                "on" => true
+            ]);
+
+        }catch (\Exception $e){
+            return response()->json([
+                "msg" => "Error al obtener datos de la API",
+                "error" => $e->getMessage()
+            ], 500);
+        }
+    }
+}
