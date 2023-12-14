24ec0f3931810dd3a54c0908b581bdfebec26375
Alonso <abrahamreynoso64@gmail.com>
2023-12-14 16:06:11 -0600

    asjhd

--- app/Http/Controllers/TempController.php
+++ app/Http/Controllers/TempController.php
@@ -224,7 +224,7 @@ class TempController extends Controller
             ->select('value', 'created_at')
             ->where('pet_device_id', $pet_device_id)
             ->whereDate('created_at', $testDate)
-            ->groupBy(DB::raw('HOUR(created_at)'))
+            ->orderBy(DB::raw('HOUR(created_at)'))
             ->get();
 
         return response()->json($values, 200);
